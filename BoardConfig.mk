f3c03d5e3e374c23a58b0e0851a3ec101f8b7a6f
thz22 <thjr5965@gmail.com>
2025-02-21 21:54:18 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,12 +24,5 @@ TARGET_SCREEN_DENSITY := 420
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
-# Kernel
-TARGET_KERNEL_CONFIG := vendor/odessa_defconfig
-
-# Kernel modules - Audio
-TARGET_MODULE_ALIASES += \
-    snd-soc-aw882xx.ko:snd_smartpa_aw882xx.ko
-
 # SELinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
